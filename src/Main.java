f609e9b3f628542349561251774b2e0cb5ba6745
luism <luismanzur91@outlook.com>
2023-05-31 17:53:44 -0400

    add generator class and auto generates the plant warehouses and producers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -10,64 +5,60 @@
  */
 
 import java.util.concurrent.Semaphore;
+import javax.swing.JOptionPane;
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Default values
+        String[] works = {"chasis", "engines", "wheels", "bodyWork", "accesories"};
+        int[] capacities = {25, 55, 35, 20, 10};
+        int[] wages = {10, 20, 8, 13, 17};
         
+        int generator = Integer.parseInt(JOptionPane.showInputDialog(null,"dveloper id last digit: "));
+        Generator generatedValues = new Generator(generator);
+        
+        Warehouse[] wareHouses = new Warehouse[works.length];
         Counter counter = new Counter(5, 1, 1);
         
-        Semaphore semaphore1 = new Semaphore(1);
-        Semaphore semaphore2 = new Semaphore(1);
-        Semaphore semaphore3 = new Semaphore(1);
-        Semaphore semaphore4 = new Semaphore(1);
-        Semaphore semaphore5 = new Semaphore(1);
+        for (int i = 0; i < works.length; i++) {
+            Semaphore semaphore = new Semaphore(1);
+            Warehouse warehouse = new Warehouse(capacities[i], works[i], counter);
+            warehouse.semaphore = semaphore;
+            wareHouses[i] = warehouse;
+        }
         
+        int quotient = generatedValues.workers / (works.length + 1);
+        int remainder = generatedValues.workers % (works.length + 1);
         
-        Warehouse chasis_warehouse = new Warehouse(10, "chasis", counter);
-        chasis_warehouse.semaphore = semaphore1;
-        Warehouse engine_warehouse = new Warehouse(10, "motor", counter);
-        engine_warehouse.semaphore = semaphore2;
-        Warehouse wheels_warehouse = new Warehouse(10, "rueda", counter);
-        wheels_warehouse.semaphore = semaphore3;
-        Warehouse bodywork_warehouse = new Warehouse(10, "carroceria", counter);
-        bodywork_warehouse.semaphore = semaphore4;
-        Warehouse accesory_warehouse = new Warehouse(10, "accesorios", counter);
-        accesory_warehouse.semaphore = semaphore5;
+        int section = 0;
+        int workersInSection = 0;
+        for (int i = 1; i <= generatedValues.workers; i++ ) {
+            if (workersInSection < quotient) {
+                Producers producer = new Producers(i,generatedValues.getWorkCapacity(works[section]),wages[section],0,works[section],true, wareHouses[section], "TRABAJANDO", true,24,2, counter);
+                producer.start();
+                workersInSection += 1;
+            } else {
+                section += 1;
+                workersInSection = 0;
+            }
+        }
+        for (int i=1; i < quotient + remainder; i++) {
+            Assemblers assembler = new Assemblers(generator - quotient + i,1,50,0,true, true, "TRABAJANDO", wareHouses[0], wareHouses[2], wareHouses[3], wareHouses[1], wareHouses[4], 2, 24, counter);
+            assembler.start();
+        }
         
-// Producers(Integer id, int capacity, Integer wage, Integer parts_produced, String type, boolean working, Warehouse warehouse, String status, boolean hired, int workday_duration, int days)
-        
-
-// Assemblers(int assembler_id, float capacity, int wage, int cars_assembled, boolean hired, boolean working, String status, 
-//            Warehouse chasis_warehouse, Warehouse wheels_warehouse, Warehouse bodywork_warehouse, Warehouse accesory_warehouse, Warehouse engine_warehouse)
-
-// Manager(int managers_id, int wage, String status, int workday_duration, int days)
-        
-// Supervisor(int days_to_shipment, Manager manager, int wage, int workday_duration, String status)
-
-        
-        Producers producer = new Producers(1,1,3,0,"chasis",true, chasis_warehouse, "TRABAJANDO", true,24,2, counter);
-        Producers producer1 = new Producers(2,1,3,0,"motor",true, engine_warehouse, "TRABAJANDO", true,24,2, counter);
-        Producers producer2 = new Producers(3,1,3,0,"carroceria",true, bodywork_warehouse, "TRABAJANDO", true,24,2, counter);
-        Producers producer3 = new Producers(4,1,3,0,"rueda",true, wheels_warehouse, "TRABAJANDO", true,24,2, counter);
-        Producers producer4 = new Producers(5,1,3,0,"accesorio",true, accesory_warehouse, "TRABAJANDO", true,24,2, counter);
-        Assemblers assembler = new Assemblers(6,1,50,0,true, true, "TRABAJANDO", chasis_warehouse, wheels_warehouse, bodywork_warehouse, engine_warehouse, accesory_warehouse, 2, 24, counter);
         
         Manager manager = new Manager(1, 40, "", 24.0, 0, counter, 2,2,2,2);
-        
         Supervisor supervisor = new Supervisor(5, manager,40, 24, "", counter);
         
-        producer.start();
-        producer1.start();
-        producer2.start();
-        producer3.start();
-        producer4.start();
         manager.start();
         supervisor.start();
-        assembler.start();
+        
         
     };
     
 };
+
