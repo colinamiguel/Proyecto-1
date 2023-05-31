cc6588e79edbb1b17ef07b34a44e4694a161cfbb
colinamiguel <miguelcolina@correo.unimet.edu.ve>
2023-05-30 23:29:02 -0400

    Carga inicial

---
+++ src/Producers.java
@@ -0,0 +1,228 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+
+public class Producers extends Thread {
+    
+    private int producer_id;
+    private int capacity;
+    private Integer wage;
+    private Integer parts_produced;
+    private String type;
+    private boolean working;
+    private Warehouse warehouse;
+    private boolean hired;
+    private String status;
+    private int workday_duration;
+    private double sleep;
+    private int days;
+    private Counter counter;
+    
+    public Producers(Integer id, int capacity, Integer wage, Integer parts_produced, String type, boolean working, Warehouse warehouse, String status, boolean hired, int workday_duration, int days, Counter counter){
+        this.producer_id = id;
+        this.capacity = capacity;
+        this.wage = wage;
+        this.parts_produced = parts_produced;
+        this.type = type;
+        this.working = working;
+        this.warehouse = warehouse;
+        this.hired = hired;
+        this.status = status;
+        this.workday_duration = workday_duration;
+        this.days = days;
+        this.counter = counter;
+    };
+
+    public int getWorkday_duration() {
+        return workday_duration;
+    };
+
+    public void setWorkday_duration(int workday_duration) {
+        this.workday_duration = workday_duration;
+    };
+    
+    public String getStatus() {
+        return status;
+    };
+
+    public void setStatus(String status) {
+        this.status = status;
+    };
+    
+    public boolean isHired() {
+        return hired;
+    };
+
+    public void setHired(boolean hired) {
+        this.hired = hired;
+    };
+
+    public Warehouse getWarehouse() {
+        return warehouse;
+    };
+
+    public void setWarehouse(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    };
+    
+
+    public int getProducer_Id() {
+        return producer_id;
+    };
+
+    public void setProducer_Id(int id) {
+        this.producer_id = id;
+    };
+
+    public float getCapacity() {
+        return capacity;
+    };
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    };
+
+    public Integer getWage() {
+        return wage;
+    };
+
+    public void setWage(Integer wage) {
+        this.wage = wage;
+    };
+
+    public Integer getParts_produced() {
+        return parts_produced;
+    };
+
+    public void setParts_produced(Integer parts_produced) {
+        this.parts_produced = parts_produced;
+    };
+
+    public String getType() {
+        return type;
+    };
+
+    public void setType(String type) {
+        this.type = type;
+    };
+
+    public boolean isWorking() {
+        return working;
+    };
+
+    public void setWorking(boolean working) {
+        this.working = working;
+    };
+    
+    @Override
+    public void run(){
+        switch(this.type){
+            case "chasis":
+                this.producer_id = producer_id;
+                this.wage = wage;
+                this.capacity = capacity;
+                this.parts_produced = 0;
+                this.type = "chasis";
+                this.working = working;
+                this.days = days;
+                this.warehouse = warehouse;
+                this.hired = hired;
+                this.status = "TRABAJANDO";
+                this.workday_duration = workday_duration;
+                this.sleep = (Double.valueOf(days)*Double.valueOf(workday_duration)/Double.valueOf(capacity))*100;
+                break;
+            case "carroceria":
+                this.producer_id = producer_id;
+                this.wage = wage;
+                this.capacity = capacity;
+                this.parts_produced = 0;
+                this.type = "carroceria";
+                this.working = working;
+                this.warehouse = warehouse;
+                this.hired = hired;
+                this.status = "TRABAJANDO";
+                this.workday_duration = workday_duration;
+                this.days = days;
+                this.sleep = (Double.valueOf(days)*Double.valueOf(workday_duration)/Double.valueOf(capacity))*100;
+                break;
+            case "motor":
+                this.producer_id = producer_id;
+                this.days = days;
+                this.wage = wage;
+                this.capacity = capacity;
+                this.parts_produced = 0;
+                this.type = "motor";
+                this.working = working;
+                this.warehouse = warehouse;
+                this.hired = hired;
+                this.status = "TRABAJANDO";
+                this.workday_duration = workday_duration;
+                this.sleep = (Double.valueOf(days)*Double.valueOf(workday_duration)/Double.valueOf(capacity))*100;
+                break;
+            case "rueda":
+                this.producer_id = producer_id;
+                this.days = days;
+                this.wage = wage;
+                this.capacity = capacity;
+                this.parts_produced = 0;
+                this.type = "rueda";
+                this.working = working;
+                this.warehouse = warehouse;
+                this.hired = hired;
+                this.status = "TRABAJANDO";
+                this.workday_duration = workday_duration;
+                this.sleep = (Double.valueOf(days)*Double.valueOf(workday_duration)/Double.valueOf(capacity))*100;
+                break;
+            case "accesorio":
+                this.producer_id = producer_id;
+                this.days = days;
+                this.wage = wage;
+                this.capacity = capacity;
+                this.parts_produced = 0;
+                this.type = "accesorio";
+                this.working = working;
+                this.warehouse = warehouse;
+                this.hired = hired;
+                this.status = "TRABAJANDO";
+                this.workday_duration = workday_duration;
+                this.sleep = (Double.valueOf(days)*Double.valueOf(workday_duration)/Double.valueOf(capacity))*100;
+                break;
+        };
+        
+        
+        while(this.hired){
+            try {
+                System.out.println("El trabajador " + this.producer_id + " está " + this.status);
+                sleep((long) this.sleep);
+                this.warehouse.semaphore.acquire(1);
+                if(this.warehouse.capacity - this.warehouse.available_parts >= 1){
+                    this.warehouse.store_parts(1);
+                    System.out.println("Se ha agregado una parte al almacén de " + this.type);
+                    System.out.println("Las partes disponibles en el almacén de " + this.type + " es de " + this.warehouse.available_parts);
+                    this.warehouse.update_status("La capacidad disponible del almacén de " + this.warehouse.type + " es de " + String.valueOf(this.warehouse.capacity - this.warehouse.available_parts) + " partes.");
+                    //System.out.println(this.warehouse.status);
+                    this.warehouse.semaphore.release();
+                }else{
+                    this.status = "OCIOSO";
+                    System.out.println("No se pudo almacenar una nueva parte en el alacén de: " + this.warehouse.type);
+                    this.warehouse.semaphore.release();
+                
+            };
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Producers.class.getName()).log(Level.SEVERE, null, ex);
+            };
+            
+        };
+    };
+    
+};
---
+++ src/Supervisor.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Supervisor extends Thread{
+    
+    public int days_to_shipment;
+    private Manager manager;
+    private int wage;
+    private int workday_duration;
+    private String status;
+    private boolean hired;
+    private boolean supervised;
+    private int time;
+    private boolean chose_time;
+    private Counter counter;
+    
+    public Supervisor(int days_to_shipment, Manager manager, int wage, int workday_duration, String status, Counter counter){
+        this.days_to_shipment = days_to_shipment;
+        this.manager = manager;
+        this.workday_duration = workday_duration;
+        this.status = status;
+        this.hired = true;
+        this.supervised = false;
+        this.time = 0;
+        this.chose_time = false;
+        this.counter = counter;
+    };
+
+    public int getDays_to_shipment() {
+        return days_to_shipment;
+    };
+
+    public void setDays_to_shipment(int days_to_shipment) {
+        this.days_to_shipment = days_to_shipment;
+    };
+    
+    @Override
+    public void run(){
+        
+        while(hired){
+            
+            
+            //System.out.println(this.status);
+            
+            
+            if(this.manager.days == this.days_to_shipment){
+                this.status = "****************PREPARANDO ENVÍO A CONCESIONARIO********************";
+                System.out.println(this.status);
+                try {
+                    sleep(this.workday_duration);
+                    this.manager.days = 0;
+                    this.counter.update_sales_income(400000);
+                    System.out.println(this.counter.income);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Supervisor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+            }else{
+                
+                this.status = "TAREAS ADMINISTRATIVAS";
+                
+                
+                                
+                if(this.supervised == false){
+                    
+                    if (chose_time == false) {
+                         time = (int) (Math.random()*(this.workday_duration/2))*1000;
+                         chose_time = true;
+                    };              
+                   
+                    if (Double.valueOf(this.time) == this.manager.current_time) {
+                        
+                        try {
+                            sleep(400);
+                            if(this.manager.status == "VIENDO CARRERAS"){                          
+                                //System.out.println("CAGASTE");
+                                System.out.println("El director ha encontrado al gerente viendo carreras.");
+                                this.counter.wage_discount = this.counter.wage_discount + 50;
+                            }
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Supervisor.class.getName()).log(Level.SEVERE, null, ex);
+                        };
+                    }
+                    };
+                    //System.out.println("supervisando al guevon");                   
+                };
+                               
+            if(this.manager.current_time == this.workday_duration*1000){
+                this.supervised = false;
+                this.chose_time = false;
+            };
+        };
+    };
+        
+};
+    
+
